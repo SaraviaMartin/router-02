3618be9f1cb74cb1fe1d14dc2b3262da3e20ac80
Juan <saraviamartin16@hotmail.com>
2023-09-02 15:51:36 -0300

    creando el componente Router

--- src/Link.jsx
+++ src/Link.jsx
@@ -9,9 +9,19 @@ export function navigate (href) {
   
 export function Link({ target, to, ...props}){
   const handleClick = (event) => {
-    event.preventDefault()
-    navigate(to)
+    
+
+    const isMainEvent = event.button === 0 //primary click
+    const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shifKey
+    const isManageableEvent = target === undefined || target === '_self'
+
+    if(isMainEvent && isManageableEvent && !isModifiedEvent){
+      event.preventDefault()
+      navigate(to) //Navegacion con SPA 
+      window.scrollTo(0, 0)
+    }
   }
+
 
   return <a onClick={handleClick} href={to} target={target} {...props}/>
 }
