ff7e39bd00eff8c666c56530c21a8e30ca7a5c4f
Juan <saraviamartin16@hotmail.com>
2023-09-05 10:21:53 -0300

    lazy loading, and i18n added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
-import HomePage from "./pages/Home"
-import AboutPage from "./pages/About"
-import { Router } from "./Router"
-import { Route } from "./Route"
 import SearchPage from "./pages/Search"
 import Page404 from "./pages/404"
 
+import { Suspense, lazy } from "react"
+import { Router } from "./Router"
+import { Route } from "./Route"
+
+const LazyHomePage = lazy(() =>  import('./pages/Home.jsx'))
+const LazyAboutPage = lazy(() =>  import('./pages/About.jsx'))
+
 const appRoutes = [
+  {
+    path: '/:lang/about',
+    Component: LazyAboutPage
+  },
   {
     path: '/search/:query',
     Component: SearchPage
@@ -17,10 +23,12 @@ function App() {
 
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} >
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={appRoutes} defaultComponent={Page404} >
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
       
     </main>
   )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,30 @@
 import {Link} from '../Link.jsx'
-export default function AboutPage() {
 
+const i18n = {
+  es: {
+    title: "Sobre nosotros",
+    button: 'Ir a la home',
+    description: "Hola me llamo Juan y este es un clon simple de React Router"
+  },
+  en: {
+    title: "About us",
+    button: "Go to Home",
+    description: "Hi mi name is Juan and this is a simple clone of React Router"
+  }
+}
+
+const useI18n = (lang )=> {
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+    const i18n = useI18n(routeParams.lang ?? 'es')
     return (
       <>
-        <h1>Router-02-About</h1>
-        <p>Este es la seccion acerda de nosotros</p>
+        <h1>{i18n.title}</h1>
+        <p>{i18n.description}</p>
         <img src="" alt="" /><br />
-        <Link to='/'>Ir a Home</Link>
+        <Link to='/'>{i18n.button}</Link>
       </>
     )
   }
